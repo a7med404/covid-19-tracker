26103c4af129fbf97a1821ed2c58a9a21edf0848
Ahmed Ibrahim <a7med_404@outlook.com>
2020-04-26 13:09:20 +0200

    after long day finaly it si work ^_^ last edit 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,10 @@ export default {
                 content: 'width=device-width, initial-scale=1'
             },
             {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+            },
+            {
                 hid: 'description',
                 name: 'description',
                 content: process.env.npm_package_description || ''
